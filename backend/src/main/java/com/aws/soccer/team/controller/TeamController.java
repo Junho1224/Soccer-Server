920c414febbf96a8b9a01d0d3967e0def2a0ad98
NohTaeHo1 <160007694+NohTaeHo1@users.noreply.github.com>
2024-05-01 19:15:00 +0900

    .

--- backend/src/main/java/com/aws/soccer/team/controller/TeamController.java
+++ backend/src/main/java/com/aws/soccer/team/controller/TeamController.java
@@ -3,46 +3,33 @@ package com.aws.soccer.team.controller;
 import com.aws.soccer.team.model.Team;
 import com.aws.soccer.team.service.TeamServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
+@Slf4j
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequiredArgsConstructor
 @RequestMapping(path = "/api/soccer")
 @RestController
 public class TeamController {
     private final TeamServiceImpl service;
-    @GetMapping("/7")
-    public List<String> getAllByTeamName(){
-        return service.getAllByTeamName();
-    }; //1
+    private final TeamRouter router;
 
-    @GetMapping("/")
-    public List<Team> getPlayerOrderBy(){
-        return service.getPlayerOrderBy();
-    }; // 10
+    @GetMapping(path = "/search")
+    public ResponseEntity<List<Map<String, Object>>> searchPlayer(
+            @RequestParam(value = "q", required = true) String q,
+            @RequestParam(value = "playerName", required = false) String playerName,
+            @RequestParam(value = "position", required = false) String position,
+            @RequestParam(value = "teamId", required = false) String teamId
+    ) {
+        log.info("Controller searchPlayer q is {}", q);
 
-    @GetMapping("/1")
-    public List<String> getPlayerByTeamId(){
-        return service.getPlayerByTeamId();
-    }; // 12
+        List<Map<String, Object>> o = router.execute(q);
 
-    @GetMapping("/2")
-    public List<String> getTeamByNoPosition(){
-        return service.getTeamByNoPosition();
-    }; // 13
-
-    @GetMapping("/3")
-    public List<String> getTeamByDate(){
-        return service.getTeamByDate();
-    }; // 14
-
-    @GetMapping("/4")
-    public List<Team> getTeamByAverageHeight(){
-        return service.getTeamByAverageHeight();
-    }; // 22
+        return ResponseEntity.ok(o);
+    }
 }
--- backend/src/main/java/com/aws/soccer/team/service/TeamServiceImpl.java
+++ backend/src/main/java/com/aws/soccer/team/service/TeamServiceImpl.java
@@ -6,36 +6,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
 public class TeamServiceImpl implements TeamService{
 
     private final TeamRepository repository;
-
-    public List<String> getAllByTeamName(){
-        return repository.getAllByTeamName();
-    }; //1
-
-    public List<Team> getPlayerOrderBy(){
-        return repository.getPlayerOrderBy();
-    }; // 10
-
-    public List<String> getPlayerByTeamId(){
-        return repository.getPlayerByTeamId();
-    }; // 12
-
-    public List<String> getTeamByNoPosition(){
-        return repository.getTeamByNoPosition();
-    }; // 13
-
-    public List<String> getTeamByDate(){
-        return repository.getTeamByDate();
-    }; // 14
-
-    public List<Team> getTeamByAverageHeight(){
-        return repository.getTeamByHeight();
-    }; // 22
 
 
 }
